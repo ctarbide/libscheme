05a4bb1bdd691cdfbcad94bbd83b4af8e41f805c
C. Tarbide <ctarbide@tuta.io>
2024-02-09 20:01:10 +0000

    simplify scheme_eval_combination into scheme_eval

--- scheme_eval.c
+++ scheme_eval.c
@@ -25,7 +25,6 @@
 #include "scheme.h"
 
 /* locals */
-static Scheme_Object *scheme_eval_combination(Scheme_Object *comb, Scheme_Env *env);
 static Scheme_Object *eval(int argc, Scheme_Object *argv[]);
 
 void
@@ -38,61 +37,60 @@ Scheme_Object *
 scheme_eval(Scheme_Object *obj, Scheme_Env *env)
 {
 	Scheme_Object *type;
-	type = SCHEME_TYPE(obj);
 
-	if (type == scheme_symbol_type) {
-		Scheme_Object *val;
-		val = scheme_lookup_value(obj, env);
+	for (;;) {
+		type = SCHEME_TYPE(obj);
 
-		if (! val) {
-			scheme_signal_error("reference to unbound symbol: %s", SCHEME_STR_VAL(obj));
+		if (type == scheme_symbol_type) {
+			Scheme_Object *val;
+			val = scheme_lookup_value(obj, env);
+
+			if (! val) {
+				scheme_signal_error("reference to unbound symbol: %s", SCHEME_STR_VAL(obj));
+			}
+
+			return val;
+		} else if (type == scheme_pair_type) {
+			Scheme_Object *rator, *rands;
+			Scheme_Object *evaled_rands[SCHEME_MAX_ARGS];
+			Scheme_Object *fun;
+			int num_rands, i;
+			rator = scheme_eval(SCHEME_CAR(obj), env);
+			type = SCHEME_TYPE(rator);
+
+			if (type == scheme_syntax_type) {
+				return SCHEME_SYNTAX(rator)(obj, env);
+			} else if (type == scheme_macro_type) {
+				fun = (Scheme_Object *) SCHEME_PTR_VAL(rator);
+				rands = SCHEME_CDR(obj);
+				obj = scheme_apply_to_list(fun, rands);
+				continue;
+			} else {
+				rands = SCHEME_CDR(obj);
+				num_rands = scheme_list_length(rands);
+				i = 0;
+
+				while (rands != scheme_null) {
+					evaled_rands[i] = scheme_eval(SCHEME_CAR(rands), env);
+					i++;
+					rands = SCHEME_CDR(rands);
+				}
+
+				return scheme_apply(rator, num_rands, evaled_rands);
+			}
+		} else {
+			return obj;
 		}
-
-		return val;
-	} else if (type == scheme_pair_type) {
-		return scheme_eval_combination(obj, env);
-	} else {
-		return obj;
-	}
-}
-
-/* local functions */
-
-static Scheme_Object *
-scheme_eval_combination(Scheme_Object *comb, Scheme_Env *env)
-{
-	Scheme_Object *rator, *type, *rands;
-	Scheme_Object *evaled_rands[SCHEME_MAX_ARGS];
-	Scheme_Object *fun, *form;
-	int num_rands, i;
-	rator = scheme_eval(SCHEME_CAR(comb), env);
-	type = SCHEME_TYPE(rator);
-
-	if (type == scheme_syntax_type) {
-		return SCHEME_SYNTAX(rator)(comb, env);
-	} else if (type == scheme_macro_type) {
-		fun = (Scheme_Object *) SCHEME_PTR_VAL(rator);
-		rands = SCHEME_CDR(comb);
-		form = scheme_apply_to_list(fun, rands);
-		return scheme_eval(form, env);
-	} else {
-		rands = SCHEME_CDR(comb);
-		num_rands = scheme_list_length(rands);
-		i = 0;
-
-		while (rands != scheme_null) {
-			evaled_rands[i] = scheme_eval(SCHEME_CAR(rands), env);
-			i++;
-			rands = SCHEME_CDR(rands);
-		}
-
-		return scheme_apply(rator, num_rands, evaled_rands);
 	}
 }
 
 static Scheme_Object *
 eval(int argc, Scheme_Object *argv[])
 {
-	SCHEME_ASSERT((argc == 1), "eval: wrong number of args");
+	if (argc == 1) {
+		scheme_signal_error("eval: wrong number of args");
+		return NULL; /* never happens */
+	}
+
 	return scheme_eval(argv[0], scheme_env);
 }
